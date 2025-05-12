c06af30f13548acec6dddf67f19239d78663d502
Bernd Kapeller <oldboy@145.127.EARLY-REGISTRATION.of.SURFnet.invalid>
2025-05-12 15:28:42 +0200

    adding new traceloop API key

--- app/observability/index.ts
+++ app/observability/index.ts
@@ -4,7 +4,7 @@ import * as LlamaIndex from "llamaindex";
 export const initObservability = () => {
   traceloop.initialize({
     appName: "llama-app-production",
-    apiKey:"8ad82fe7bc1db05540ad408232fb6499ee65a650f35599412337f6dcf3b7121233c35cc141afe37cde496cfb1544bb9f",
+    apiKey:"tl_fc1e0f89af3348e8adce9eb66e21ad07",
     disableBatch: true,
     instrumentModules: {
       llamaIndex: LlamaIndex,
